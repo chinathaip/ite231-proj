f6a250bf00a93753c9a1f898c5bfc5704845c7df
chinathaip <cartoonabe@gmail.com>
2024-02-10 21:27:05 +0700

    create folder and files at root dir

--- user-group.sh
+++ user-group.sh
@@ -15,12 +15,12 @@ useradd -G backofthehouse sompong
 useradd -G backofthehouse sommhai
 useradd -G backofthehouse somying
 
-mkdir menus
-touch menus/chicken_wonton.txt
-touch menus/spicy_wonton.txt
-touch menus/crispy_wonton.txt
+mkdir ~/menus
+touch ~/menus/chicken_wonton.txt
+touch ~/menus/spicy_wonton.txt
+touch ~/menus/crispy_wonton.txt
 
-mkdir recipes
-touch recipes/chicken_wonton_manual.pdf
-touch recipes/spicy_wonton_manual.pdf
-touch recipes/crispy_wonton_manual.pdf
+mkdir ~/recipes
+touch ~/recipes/chicken_wonton_manual.pdf
+touch ~/recipes/spicy_wonton_manual.pdf
+touch ~/recipes/crispy_wonton_manual.pdf
