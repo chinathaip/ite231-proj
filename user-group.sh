66d4023b3077950cbbd060dcaaafc504ceff1963
kevinisthicc <Krittiwitdooo@gmail.com>
2024-02-10 21:09:14 +0700

    dodo frontandbackofthehouse

--- user-group.sh
+++ user-group.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 #
 # Script for user and group goes here
+groupadd frontofthehouse
+useradd -G frontofthehouse bob
+useradd -G frontofthehouse tedison
+useradd -G frontofthehouse benedict
+useradd -G frontofthehouse john
+useradd -G frontofthehouse peter
+
+
+groupadd backofthehouse
+useradd -G backofthehouse somchai
+useradd -G backofthehouse somsri
+useradd -G backofthehouse sompong
+useradd -G backofthehouse sommhai
+useradd -G backofthehouse somying
