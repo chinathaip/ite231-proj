7e29462652637fd7513a5ebf08569c0ed155c273
chinathaip <cartoonabe@gmail.com>
2024-02-10 21:13:32 +0700

    add menus and recipes folder

--- user-group.sh
+++ user-group.sh
@@ -8,10 +8,19 @@ useradd -G frontofthehouse benedict
 useradd -G frontofthehouse john
 useradd -G frontofthehouse peter
 
-
 groupadd backofthehouse
 useradd -G backofthehouse somchai
 useradd -G backofthehouse somsri
 useradd -G backofthehouse sompong
 useradd -G backofthehouse sommhai
 useradd -G backofthehouse somying
+
+mkdir menus
+touch menus/chicken_wonton.txt
+touch menus/spicy_wonton.txt
+touch menus/crispy_wonton.txt
+
+mkdir recipes
+touch recipes/chicken_wonton_manual.pdf
+touch recipes/spicy_wonton_manual.pdf
+touch recipes/crispy_wonton_manual.pdf
