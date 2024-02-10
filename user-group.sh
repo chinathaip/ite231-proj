fea87bfa1d327b449e3d2952757d9e7d007d556f
chinathaip <cartoonabe@gmail.com>
2024-02-10 22:51:26 +0700

    format management

--- user-group.sh
+++ user-group.sh
@@ -15,38 +15,39 @@ useradd -G backofthehouse sompong
 useradd -G backofthehouse sommhai
 useradd -G backofthehouse somying
 
-mkdir /frontofthehouse
-mkdir /frontofthehouse/menus
-touch /frontofthehouse/menus/chicken_wonton.txt
-touch /frontofthehouse/menus/spicy_wonton.txt
-touch /frontofthehouse/menus/crispy_wonton.txt
-
-mkdir /backofthehouse
-mkdir /backofthehouse/recipes
-touch /backofthehouse/recipes/chicken_wonton_manual.pdf
-touch /backofthehouse/recipes/spicy_wonton_manual.pdf
-touch /backofthehouse/recipes/crispy_wonton_manual.pdf
-
-chown :frontofthehouse /frontofthehouse
-chmod 770 /frontofthehouse
-
-chown :backofthehouse /backofthehouse
-chmod 770 /backofthehouse
-
 groupadd management
 useradd -G management chinathai
 useradd -G management watsamon
 useradd -G management krittawit
 useradd -G management phachathakorn 
 useradd -G management karnsiree
-#
-#mkdir storage 
-#mkdir employee 
-#mkdir transaction
-#touch storage/rawchicken.csv
-#touch storage/vegetables.csv
-#touch stoage/sauces.csv
-#touch employee/employee_list.txt
-#touch transaction/20230210.csv
-#touch transaction/20230209.csv
-#touch transaction/20230208.csv
+
+mkdir /frontofthehouse
+mkdir /frontofthehouse/menus
+touch /frontofthehouse/menus/chicken_wonton.txt
+touch /frontofthehouse/menus/spicy_wonton.txt
+touch /frontofthehouse/menus/crispy_wonton.txt
+chown :frontofthehouse /frontofthehouse
+chmod 770 /frontofthehouse
+
+mkdir /backofthehouse
+mkdir /backofthehouse/recipes
+mkdir /backofthehouse/storage
+touch /backofthehouse/recipes/chicken_wonton_manual.pdf
+touch /backofthehouse/recipes/spicy_wonton_manual.pdf
+touch /backofthehouse/recipes/crispy_wonton_manual.pdf
+touch /backofthehouse/storage/rawchicken.csv
+touch /backofthehouse/storage/vegetables.csv
+touch /backofthehouse/storage/sauces.csv
+chown :backofthehouse /backofthehouse
+chmod 770 /backofthehouse
+
+mkdir /management
+mkdir /management/employee
+mkdir /management/transaction
+touch /management/employee/employee_list.txt
+touch /management/transaction/20230210.csv
+touch /management/transaction/20230209.csv
+touch /management/transaction/20230208.csv
+chown :management /management
+chmod 770 /management
