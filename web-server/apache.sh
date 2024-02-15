d5a3a280b4604cdbb7a9e8d46176324e9023ebe5
Kidcatmeow <91112392+Kidcatmeow@users.noreply.github.com>
2024-02-15 21:26:02 +0700

    Apache.sh added

---
+++ web-server/apache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#Apache server installation
+apt-get install apache2 --yes
+
+#PHP installation
+apt-get install php libapache2-mod-php php-mysql
+
+#Create test.php file
+echo <?php echo "Hello , world! This is PHP running on Apache!" ?> >> /var/www/test.php
+
+#Start Apache2 service
+/etc/init.d/apache2 restart
+
+apt install net-tools
+
+
