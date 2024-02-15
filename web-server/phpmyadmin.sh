7528d760523182f12727e8def3213c2e295493cd
Kidcatmeow <91112392+Kidcatmeow@users.noreply.github.com>
2024-02-15 23:54:29 +0700

    add phpmyadmin sshell script

--- web-server/phpmyadmin.sh
+++ web-server/phpmyadmin.sh
@@ -3,3 +3,24 @@
 #PHPMyAdmin Installation
 sudo apt install phpmyadmin --yes
 
+#Create a New MySQL Administrator Account
+#Open the MySQL terminal and log in as the sudo (or root) user:
+sudo mysql
+
+#create a new user with a secure password
+CREATE USER 'newuser'@'localhost' IDENTIFIED BY 'password';
+
+#Assign appropriate privileges to the new user.
+GRANT ALL PRIVILEGES ON mydatabase.* TO 'newuser'@'localhost';
+
+#Flush all privileges:
+FLUSH PRIVILEGES;
+
+#Exit the MySQL terminal
+EXIT;
+
+#add phpmyadmin file path to apache
+echo Include /etc/phpmyadmin/apache.conf >> /etc/apache2/apache2.conf
+
+#restart apache server
+sudo /etc/init.d/apache2 restart
