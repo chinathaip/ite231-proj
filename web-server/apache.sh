3d3be2c6ee0da875b0c89225a731fecfa580ec04
Kidcatmeow <91112392+Kidcatmeow@users.noreply.github.com>
2024-02-15 22:55:18 +0700

    apache maintenance added

--- web-server/apache.sh
+++ web-server/apache.sh
@@ -12,5 +12,11 @@ echo <?php echo "Hello , world! This is PHP running on Apache!" ?> >> /var/www/t
 #Start Apache2 service
 /etc/init.d/apache2 restart
 
+#maintenance
+#Hiding Apache version and OS information
+echo Include /etc/apache2/httpd.conf >> /etc/apache2/apache2.conf
 
+echo ServerSignature Off \n ServerTokens Prod >> /etc/apache2/httpd.conf
 
+#Restart Apache2 service , now apache version and OS information should disappear
+/etc/init.d/apache2 restart
